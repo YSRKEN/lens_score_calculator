6cb2f72b4c26e778c4f4dfb2d1d657af196d34e1
YSRKEN <ysr.ken@gmail.com>
2020-02-19 20:44:03 +0900

    [add] データをスクレイピングして取得するエンドポイントを追加した

--- server/main.py
+++ server/main.py
@@ -1,13 +1,20 @@
+import codecs
+import json
+import re
 import sqlite3
 from contextlib import closing
+from pprint import pprint
+from typing import List, Dict
 
 from flask import Flask, jsonify
 from flask_cors import CORS
+from requests_html import HTMLSession, HTML, Element
 
 DB_PATH = 'lens_data.db'
 
 app = Flask(__name__)
 CORS(app)
+session = HTMLSession()
 
 
 @app.route('/')
@@ -94,6 +101,51 @@ def get_lens_score(lens_id: str, data_type: str, f_value_type: str):
                     'score': record[1]
                 })
         return jsonify(score_list)
+
+
+@app.route('/api/lenses/<lens_id>/pre')
+def get_pre_scores(lens_id: str):
+
+    page = session.get(f'https://www.opticallimits.com/m43/{lens_id}?start=1')
+    if not page.ok:
+        return jsonify({'result': 'ng'})
+    with open('temp.html', 'w', encoding='utf-8') as f:
+        f.write(page.text)
+    html: HTML = page.html
+
+    img_list: List[Element] = html.find('img')
+    for img_tag in img_list:
+        url = 'https://www.opticallimits.com' + img_tag.attrs['src']
+        if 'mtf.png' in url:
+            return jsonify({'result': 'ok', 'type': 'image', 'data': url})
+
+    m = re.search(r'loadCharts\((\d+)\);', page.text)
+    lens_id_inner = m.group(1)
+    page2 = session.post(f'https://www.opticallimits.com/js/php/get_data.php', data={
+        'lens_id': lens_id_inner
+    })
+    if not page2.ok:
+        return jsonify({'result': 'ng'})
+    resp: Dict[str, any] = json.loads(page2.text.encode().decode('utf-8-sig'))
+    responst_json = ({'result': 'ok', 'type': 'text', 'data': []})
+    for focal_length, data in resp.items():
+        data: Dict[str, List[any]] = data
+        if 'mtf_val_extreme' in data:
+            center_data: List[int] = data['mtf_val_center']
+            edge_data: List[int] = data['mtf_val_extreme']
+        else:
+            center_data: List[int] = data['mtf_val_center']
+            edge_data: List[int] = data['mtf_val_border']
+        f_value_list: List[str] = [x.replace('F/', '') for x in data['ca_cat']]
+        for f_value, center_score, edge_score in zip(f_value_list, center_data, edge_data):
+            responst_json['data'].append({
+                'focal': focal_length,
+                'f': f_value,
+                'center': center_score,
+                'edge': edge_score
+            })
+
+    return jsonify(responst_json)
 
 
 if __name__ == '__main__':
